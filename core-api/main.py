a84f70f96240d040d8387d1ac528884c1ca05087
ademismkv <ademi.smkv@gmail.com>
2025-05-23 19:45:21 +0600

    fix

--- core-api/main.py
+++ core-api/main.py
@@ -645,3 +645,8 @@ async def debug():
                 "traceback": traceback.format_exc().split("\n")
             }
         ) 
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False) 
